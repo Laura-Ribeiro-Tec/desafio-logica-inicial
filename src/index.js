f62a42b485bae63d8b0bdcdf91ac41a97b801754
Laura-Ribeiro-Tec <lauraribeirotec@gmail>
2026-01-09 15:17:27 -0300

    utiliza estrutura de repetição

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 
 
-let nickname = "Picachu"
-let nivel = "Iniciante"
-let qtdExperiencia = 2000
+const herois = [
+    {nome: "Picachu", xp: 2000},
+    {nome: "Charmander", xp: 4500},
+    {nome: "bulbasaur", xp: 12000}
+];
 
-console.log(`Bem vindo ${nickname}`);
+for(let i = 0; i < herois.length; i++){
+    let nivel = "";
+    const xp = herois[i].xp;
 
+    if(xp < 1000){
+        nivel = "Ferro";
+    } else if(xp <= 2000){
+        nivel = "Bronze";
+    } else if(xp <= 5000){
+        nivel = "Prata";
+    } else if(xp <= 7000){
+        nivel = "Ouro";
+    } else if(xp <= 8000){
+        nivel = "Platina";
+    } else if(xp <= 9000){
+        nivel = "Ascendente";
+    } else if(xp <= 10000){
+        nivel = "Imortal";
+    } else{
+        nivel = "Radiante";
+    }
 
-if(qtdExperiencia < 1000){
-    nivel = "Ferro";
-} else if(qtdExperiencia <= 2000){
-    nivel = "Bronze";
-} else if(qtdExperiencia <= 5000){
-    nivel = "Prata";
-} else if(qtdExperiencia <= 7000){
-    nivel = "Ouro";
-} else if(qtdExperiencia <= 8000){
-    nivel = "Platina";
-} else if(qtdExperiencia <= 9000){
-    nivel = "Ascendente";
-} else if(qtdExperiencia <= 10000){
-    nivel = "Imortal";
-} else{
-    nivel = "Radiante";
+    console.log(`O herói de nome ${herois[i].nome} está no nível de ${nivel}`);
 }
 
-console.log(`O herói de nome ${nickname} está no nível de ${nivel}`);
+
+
+
+
 
 
 
